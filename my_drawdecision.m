da8f5ea42f1f1aa0ff7268bfbe4b9b49d4151c23
Igelse <38281276+Igelse@users.noreply.github.com>
2022-07-21 16:26:28 +0200

    Update my_drawdecision.m

--- my_drawdecision.m
+++ my_drawdecision.m
@@ -9,8 +9,6 @@ pos1 = xpos(r(1));
 pos2 = xpos(r(2));
 
 
-DrawFormattedText(w, 'Press the right button', rect(3)/1.6, rect(4)/3, white);
-DrawFormattedText(w, 'Press the left button', rect(3)/6, rect(4)/3, white);
 DrawFormattedText(w, 'Human', pos1+85, 'center', white);
 DrawFormattedText(w, 'Robot', pos2+85, 'center', white);
 Screen('Flip', w);
@@ -23,5 +21,6 @@ while keyIsDown == false | ismember(KbName(keyCode), key_set) == 0;
 [keyIsDown, secs, keyCode, deltaSecs] = KbCheck;
 end
 response = KbName(keyCode); % unified key name
+
 
 end
